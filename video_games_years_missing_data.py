3200c11c4ca0c105820996624ad0f76be4f961fc
MZinko <michalzinko@gmail.com>
2024-06-27 18:23:41 +0200

    fix:loop_that finds_years_of_relese_deleting_dictionary

--- video_games_years_missing_data.py
+++ video_games_years_missing_data.py
@@ -5,17 +5,13 @@ import csv
 
 # loop that creates dictionary of csv file
 # open csv file
-with open('Video_games_missing_years_test.csv','r') as csv_file:
+with open('Video_games_missing_years.csv','r') as csv_file:
     content = csv.reader(csv_file)
-    games_dictionary = {}
     list_of_games_names = []
-    list_of_platforms = []
     for line in content:
         if line[0] != 'Name':
             if line[1] != 'Platform':
-                games_dictionary[line[0].strip()] = line[1].strip() 
                 list_of_games_names.append(line[0].strip())
-                list_of_platforms.append(line[1].strip())
 #for loop that creates urls from dictionary file
 
 # getting games names 
@@ -23,20 +19,17 @@ count_done = 0
 count = 0
 for game_name in list_of_games_names:
     game_platform = games_dictionary[game_name]
-    url_game_name = game_name.lower().replace(' ', '-').replace(':', '').replace('.', '').replace('ii', '').replace("'", '').replace('!', '').replace('fifa-soccer', 'fifa')
+    url_game_name = game_name.lower().replace(' ', '-').replace(':', '').replace('.', '').replace("'", '').replace('!', '').replace('/', '').replace('fifa-soccer', 'fifa')
     url = f'https://rawg.io/games/{url_game_name}'
-    print(f'\n{url}')
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'lxml')
-    infos = soup.find_all('div', class_ = "game__meta-text", itemprop="datePublished")
-    for info in infos: 
-        if info == None:
-            print('N/A')
-        else:
-            date_release = info.text
-            lenght_of_date = len(date_release)
-            year = date_release[lenght_of_date-4::]
-            print(year)
-            count_done += 1
-        count += 1
-print(count_done, count)
+    info = soup.find('div', class_ = "game__meta-text", itemprop="datePublished")
+    if info == None:
+        print(game_name, 'N/A')
+    else:
+        count_done += 1
+        date_release = info.text
+        lenght_of_date = len(date_release)
+        year = date_release[lenght_of_date-4::]
+    count += 1
+print(count_done/count * 100%)
